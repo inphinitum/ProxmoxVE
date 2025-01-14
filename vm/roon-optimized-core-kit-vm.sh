8e62a1794633f3869df0ffaa941d3b3b830de036
Javier Martínez <javi@flamingalah.net>
2025-01-15 00:44:32 +0100

    add sleep to allow for print

--- vm/roon-optimized-core-kit-vm.sh
+++ vm/roon-optimized-core-kit-vm.sh
@@ -435,6 +435,7 @@ qm create $VMID -agent 1${MACHINE} -tablet 0 -localtime 1 -bios ovmf${CPU_TYPE} 
   -name $HN -tags proxmox-helper-scripts -net0 virtio,bridge=$BRG,macaddr=$MAC$VLAN$MTU -onboot 1 -ostype l26 -scsihw virtio-scsi-pci
 pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 msg_info "qm importdisk $VMID ${FILE%.*} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null"
+sleep 2
 qm importdisk $VMID ${FILE%.*} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
